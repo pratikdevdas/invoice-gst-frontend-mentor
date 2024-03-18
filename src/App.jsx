5d1b0e7a9d9f86631fe25253621af2624ec9a380
Revenant <dasdev.pratik@gmail.com>
2024-03-19 04:56:09 +0530

    🐛 (fixed routes handling ): Route handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence } from 'framer-motion'
 import { Routes, Route, useLocation } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 // import Center from './components/invoices/Center'
 // import Header from './components/Header'
 import OrderInfo from './components/orders/OrderInfo'
@@ -9,10 +9,12 @@ import Products from './components/products/Products'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import LiveInvoice from './components/live/LiveInvoice'
+import { selectCurrentToken } from './redux/authSlice'
 
 function App() {
   const location = useLocation()
   const dispatch = useDispatch()
+  const token = useSelector(selectCurrentToken)
 
   const onDelete = (id) => {
     dispatch(invoiceSlice.actions.deleteInvoice({ id }))
@@ -23,12 +25,17 @@ function App() {
 
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          <Route element={<Dashboard />} path="/dashboard">
-            <Route element={<Products />} path="products" />
-            <Route element={<LiveInvoice />} path="live" />
-            <Route element={<OrderInfo onDelete={onDelete} />} path="order" />
-          </Route>
-          <Route element={<Login />} path="/" />
+          {token ? (
+            <>
+              <Route element={<Dashboard />} path="/" />
+              <Route element={<Dashboard />} path="/dashboard">
+                <Route element={<Products />} path="products" />
+                <Route element={<LiveInvoice />} path="live" />
+                <Route element={<OrderInfo onDelete={onDelete} />} path="order" />
+              </Route>
+            </>
+          )
+            : <Route element={<Login />} path="/" />}
         </Routes>
       </AnimatePresence>
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,14 @@
-import { useLocation, Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useLocation, Outlet } from 'react-router-dom'
 import Header from './Header'
 import Center from './orders/Center'
-import { selectCurrentToken } from '../redux/authSlice'
 
 function Dashboard() {
   const location = useLocation()
-  const token = useSelector(selectCurrentToken)
-  console.log(token)
+
   return (
     <div className="dark:bg-[#141625] scrollbar-hide duration-300 min-h-screen bg-[#f8f8fb] py-[34px] px-2 md:px-8 lg:px-12 lg:py-[72px]">
       <Header />
-      {token ? <Center /> : <Navigate to="/login" state={{ from: location }} replace />}
-      {/* {location.pathname === '/dashboard' ? <Center /> : <Outlet />} */}
+      {location.pathname === '/dashboard' ? <Center /> : <Outlet />}
     </div>
   )
 }
